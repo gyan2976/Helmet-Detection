69a2221678ef0878c38af05d6870caa7f226f1cd
gyan2976 <gyankumar2431@gmail.com>
2025-02-16 13:25:31 +0530

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="helmet",  # Name of your root folder
+    version="0.0.1",
+    author="Gyan",
+    author_email="gyankumar2431@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
