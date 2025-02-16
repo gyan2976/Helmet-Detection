2b8766ae04637ff95f439d63681d926c45056c9a
gyan2976 <gyankumar2431@gmail.com>
2025-02-16 13:52:59 +0530

    checking log

--- checklog.py
+++ checklog.py
@@ -1,3 +1,11 @@
 from helmet.logger import logging
+from helmet.exception import HelmetException
+import sys
 
 logging.info("Hi Gyan")
+
+try:
+    a = 7 + 'Gyan'
+    print(a)
+except Exception as e:
+    raise HelmetException(e, sys) from e
